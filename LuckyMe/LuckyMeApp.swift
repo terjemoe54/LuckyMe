e0c7f2d4307fc216399130797244a8d0b12fd006
terjemoe54 <102929721+terjemoe54@users.noreply.github.com>
2024-01-07 10:31:19 +0100

    Initial Commit

---
+++ LuckyMe/LuckyMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuckyMeApp.swift
+//  LuckyMe
+//
+//  Created by Terje Moe on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LuckyMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
