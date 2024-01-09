fe13efd5a90bd7fd624c324c1ec537f00b1461b0
terjemoe54 <102929721+terjemoe54@users.noreply.github.com>
2024-01-09 09:12:16 +0100

    -lagt til array

--- LuckyMe/ContentView.swift
+++ LuckyMe/ContentView.swift
@@ -8,17 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var pnr: String = ""
+    // var nrArray = [String]()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Person Nr:").font(.largeTitle)
+            
+            TextField("Fødselsnummer 11 siffer ",text: $pnr)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .border(Color.black)
+            
+            Button{
+                
+             mod11()
+                
+           } label: {
+                Text("Modulus 11")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                
+            }
+            Text("Pnr:\(pnr) \(pnr)")
         }
         .padding()
     }
+    
+    func mod11(){
+       let str = pnr
+        var myArray:[String] = []
+        // let index = str.index(str.startIndex, offsetBy: 0)
+
+        for (index, element) in str.enumerated(){
+            print("Item \(index): \(element)")
+            var pnr1 = "pnr" + String(index)
+            myArray.append(String(element))
+           
+        }
+   let a = myArray[1]
+       
+    }
 }
 
-#Preview {
+    #Preview {
     ContentView()
 }
