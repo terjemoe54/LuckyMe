4c4d038b9eb0e075bb26394e856b9cf11400a9e9
terjemoe54 <102929721+terjemoe54@users.noreply.github.com>
2024-01-09 16:31:24 +0100

    -utregning av konrollsiffer 1 og 2

--- LuckyMe/ContentView.swift
+++ LuckyMe/ContentView.swift
@@ -10,8 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State var myArray:[String] = []
     @State var pnr: String = ""
-   
-    
+ 
     var body: some View {
         VStack {
             Text("Person Nr:").font(.largeTitle)
@@ -21,33 +20,30 @@ struct ContentView: View {
                 .border(Color.black)
             
             Button{
-                
             fillArray()
             arrToVar()
-                
            } label: {
                 Text("Modulus 11")
                     .padding()
                     .foregroundColor(.white)
                     .background(Color.blue)
-                
             }
-            Text("Pnr:\(pnr) \(pnr)")
+          //  Text("Pnr:\(pnr) \(pnr)")
         }
         .padding()
     }
     
     func fillArray(){
+        myArray.removeAll()
         let str = pnr
               
-        for (index, element) in str.enumerated(){
+        for (_, element) in str.enumerated(){
             myArray.append(String(element))
         }
     }
      
     func arrToVar() {
-     // Konverterer alle tall fra arrayen til variabler av type Integer
-        
+        // Konverterer alle tall fra arrayen til variabler av type Integer
         let a1 = Int(myArray[0])
         let a2 = Int(myArray[1])
         let a3 = Int(myArray[2])
@@ -57,33 +53,47 @@ struct ContentView: View {
         let a7 = Int(myArray[6])
         let a8 = Int(myArray[7])
         let a9 = Int(myArray[8])
-        let a10 = Int(myArray[9])
-        let a11 = Int(myArray[10])
-
+  
         // Utregning første kontrollsiffer til modulus 11
-        
-        var fks1 = a1! * 3
-        var fks2 = a2! * 7
-        var fks3 = a3! * 6
-        var fks4 = a4! * 1
-        var fks5 = a5! * 8
-        var fks6 = a6! * 9
-        var fks7 = a7! * 4
-        var fks8 = a8! * 5
-        var fks9 = a9! * 2
-        var sum1 = fks1 + fks2 + fks3 + fks4 + fks5 + fks6 + fks7 + fks8 + fks9
-        var sum2 = sum1 / 11
-        var sum3 = 11 * sum2
-        var sum4 = sum1 - sum3
+        let fks1 = a1! * 3
+        let fks2 = a2! * 7
+        let fks3 = a3! * 6
+        let fks4 = a4! * 1
+        let fks5 = a5! * 8
+        let fks6 = a6! * 9
+        let fks7 = a7! * 4
+        let fks8 = a8! * 5
+        let fks9 = a9! * 2
+        let sum1 = fks1 + fks2 + fks3 + fks4 + fks5 + fks6 + fks7 + fks8 + fks9
+        let sum2 = sum1 / 11
+        let sum3 = 11 * sum2
+        let sum4 = sum1 - sum3
         var ks1 = 11 - sum4
         if ks1 == 11 {
             ks1 = 0
         }
-        print(ks1)
+        // Utregning andre kontrollsiffer til modulus 11
+        let sks1 = a1! * 5
+        let sks2 = a2! * 4
+        let sks3 = a3! * 3
+        let sks4 = a4! * 2
+        let sks5 = a5! * 7
+        let sks6 = a6! * 6
+        let sks7 = a7! * 5
+        let sks8 = a8! * 4
+        let sks9 = a9! * 3
+        let sks10 = ks1 * 2
+        let skSum1 = sks1 + sks2 + sks3 + sks4 + sks5 + sks6 + sks7 + sks8 + sks9 + sks10
+        let skSum2 = skSum1 / 11
+        let skSum3 = skSum2 * 11
+        let skSum4 = skSum1 - skSum3
+        var ks2 = 11 - skSum4
+        if pnr.count < 11 {
+            pnr = pnr + String(ks1) + String(ks2)
+        }
     }
  }
-
  
-#Preview {
+ #Preview {
     ContentView()
-}
+ }
